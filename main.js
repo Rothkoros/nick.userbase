1114cbb9759b06d6ed194e02c7e7acad374c437a
Devin Curtis <devincurtis@UA105350927.localdomain>
2020-05-06 23:43:14 -0600

    done

--- main.js
+++ main.js
@@ -118,7 +118,6 @@ app.post("/addUser", (req, res) => {
     };
     client.query(searchQuery, (err, response) => {
       if (err) throw err;
-      console.log("RESPONSE.ROWS", response.rows[0]);
       res.render("results", { userData: response.rows[0] });
     });
   }
