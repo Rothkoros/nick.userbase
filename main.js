a563485b16ddf4fa3e38658db1fa5b4a96834d25
Devin Curtis <devincurtis@UA105350927.localdomain>
2020-05-06 21:20:29 -0600

    Fixed table creation string

--- main.js
+++ main.js
@@ -13,10 +13,10 @@ client.connect();
 const createTableString = `
 CREATE TABLE IF NOT EXISTS users (
   id varchar(50),
-  firstName varchar(50),
-  lastName varchar(50),
+  first_name varchar(50),
+  last_name varchar(50),
   email varchar(50),
-  birthDate varchar(8),
+  birth_date varchar(8),
   password varchar(15)
 );
 `;
